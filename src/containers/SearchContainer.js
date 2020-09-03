d57f38b4fdfb1e7896f0416c75176e01a3b360b9
James Scolamieri <james.scolamieri@gmail.com>
2020-09-03 12:43:34 -0700

    containers set up process, imported Provider to index.js to add containers to components

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -2,11 +2,19 @@ import { connect } from 'react-redux';
 import Search from './../components/Search.js';
 import handleSearchChange from '../actions/search.js';
 
-var SearchContainer = () => {};
+// var SearchContainer = () => {
+// };
 
 //TODO: define a SearchContainer component which will hook up your action
 // dispatchers with your search component props.
 //HINT: use react-redux 'connect' method to generate a container component from
 //state and dispatch mappings.
+
+var SearchContainer = connect(
+  handleSearchChange
+)(Search);
+
+// Give VoteComponent the props it expects, votes + updateVote
+//containers, connect actions to components
 
 export default SearchContainer;
